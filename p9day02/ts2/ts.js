a03de2e1d2f1cd64953460b0bf7073bffabdf4a5
jikljunecnc <109512580+jikljunecnc@users.noreply.github.com>
2022-07-26 09:59:15 +0800

    Add files via upload

---
+++ p9day02/ts2/ts.js
@@ -0,0 +1,27 @@
+"use strict";
+// function greeter(person: string) {
+//     return "Hello, " + person;
+// }
+// let user = "Jane";
+// document.body.innerHTML = greeter(user);
+// let b=["贾博林"]
+// console.log(b);
+// document.body.innerHTML = "贾博林";
+// let aa=[1+7]
+// let bb=[2+3]
+// console.log(aa);
+// console.log(bb);
+// let num1 = 1
+// let num2 = 2
+// let sum = num1 + num2 
+// console.log(sum);
+// enum Color {
+//     /**红色 */
+//     Red = 1,
+//     /**蓝色 */
+//     Blue = 2,
+//     /**绿色 */
+//     Green = 3
+// }
+// let c: Color = Color.Green;
+// console.log(c) // 3
